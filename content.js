4df87f56339fff2a44dd84c2ca1203588bdd41dc
Rich Boakes <rjb@port.ac.uk>
2023-11-18 17:50:07 +0000

    basic work

--- content.js
+++ content.js
@@ -15,6 +15,7 @@ function walk(el) {
 		el.parentNode.removeChild(el);
 		node.outerHTML = html;
 	} else {
+		console.log("x", el);
 		const kids = Array.from(el.childNodes)
 		for (const child of kids) {
 			walk(child);
@@ -29,46 +30,56 @@ function escapeHTML(text) {
 }
 
 function randomStyle(c) {
-	return `letter-spacing: ${spacing(c)}; font-family: ${rndFont(c)}; ${shadow()}; ${rndbg()}; transform: rotatez(${Math.random()*20-10}deg) rotatex(${Math.random()*20-10}deg) rotatey(${Math.random()*20-10}deg) ${rndTranslate()}`;
+	return `${extraSpace(c)}; ${rndBlur()}; ${rndFont(c)}; ${shadow()}; ${rndbg()}; ${rndTransform()}`;
 }
 
-function spacing(c) {
-	if (c === " ") {
-		console.log("space");
-		return `2em`;
+function rndTransform() {
+	return `transform: scale(0.9) ${rndTranslate()} rotateY(${Math.random()*80-40}deg) rotateX(${Math.random()*8-4}deg) rotateZ(${Math.random()*16-8}deg)`;
+}
+
+function extraSpace(c) {
+	if (c.endsWith(" ")) {
+		return `margin-right: 0.5em;`
 	} else {
-		return `0`;
+		return ``
 	}
 }
-
 
 function rndTranslate() {
 	return `translate(${Math.random(60)}px, ${Math.random(60)}px)`
 }
 
 function shadow() {
-	return `box-shadow: ${Math.random()*3}px ${Math.random()*3}px ${Math.random()*3}px black`
+	return `box-shadow: ${Math.random()*3}px ${Math.random()*3}px ${3+Math.random()*3}px rgba(0,0,0,0.3);`
 }
 
 const commonFonts = [
-	"Arial",
-	"Verdana",
-	"Helvetica",
-	"Tahoma",
-	"Trebuchet MS",
-	"Times New Roman",
-	"Georgia",
-	"Garamond",
-	"Courier New",
-	"Brush Script MT",
-	"Lucida Sans",
-	"Palatino",
-	"Bookman",
-	"Avant Garde",
-	"Schoolbook",
+	"Andale Mono",
 	"Arial Black",
+	"Arial",
+	"Avant Garde",
+	"Bookman",
+	"Brush Script MT",
 	"Comic Sans MS",
+	"Courier New",
+	"Engagement",
+	"Garamond",
+	"Georgia",
+	"Helvetica Neue",
+	"Helvetica",
+	"Hoefler Text",
 	"Impact",
+	"Lucida Grande",
+	"Lucida Sans",
+	"Monaco",
+	"Optima",
+	"Palatino",
+	"Plaster",
+	"Schoolbook",
+	"Tahoma",
+	"Times New Roman",
+	"Trebuchet MS",
+	"Verdana"
   ];
 
 const availableFonts = commonFonts.filter(isFontAvailable);
@@ -91,12 +102,20 @@ function isFontAvailable(fontName) {
 
 function rndFont(character) {
     let font = availableFonts[Math.floor(Math.random()*availableFonts.length)];
-    return font;
+    return `font-family: ${font};`;
 }
 
+function rndBlur() {
+    return `filter: blur(${Math.random()*1}px)`;
+}
+
+
 function rndbg() {
-	const bg = 205+Math.random()*50;
-	return `background-color: rgb(${bg}, ${bg}, ${bg});`
+	const r = 205+Math.random()*50;
+	const g = 205+Math.random()*50;
+	const b = 205+Math.random()*50;
+	return `background-color: rgb(${r}, ${g}, ${b});`;
+//	return `background-color: rgb(${r}, ${g}, ${b}); background-image: url("data:image/svg+xml,%3C!-- svg: first layer --%3E%3Csvg viewBox='0 0 250 250' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='4' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`;
 }
 
 walk(document.body);
