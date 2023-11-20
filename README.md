00ffa53b9542d4e6fef089831f99acac0ac23a99
Rich Boakes <rjb@port.ac.uk>
2023-11-20 14:41:24 +0000

    a silly-interfaces-hackday code drop

---
+++ README.md
@@ -0,0 +1,5 @@
+# RANSOM NOTE EXTENSION
+
+Have you over wondered what the world would look like if every website were designed like a 1960s ransom letter?  Well, wonder no more with this free-to-own browser extension.  Hours of fun are literally non-guaranteeable as you look at different websites and experience in vivid reality just what it would be like if each website had been desgined by someone who forgot what they were supposed to be doing, and instead got busy with the scissors and glue.
+
+Built for a hackday.  Thanks to [Port 57 Coworking Studio](https://www.port57.com/) (on Albert Road, Portsmouth) for the use of the premesis for the day.
--- content.js
+++ content.js
@@ -1,121 +1,108 @@
-console.log('Ransom Thing Running');
-console.log(document.body);
-
 function walk(el) {
-	if (el.nodeType == Node.TEXT_NODE)
-	{
-		const text = el.textContent;
-		let html = "";
-		for (const c of [...text]) {
-			html += `<span style="${randomStyle(c)}">${escapeHTML(c)}</span>`;
-		};
-		
-		const node = document.createElement("span");
-		el.parentNode.insertBefore(node, el);
-		el.parentNode.removeChild(el);
-		node.outerHTML = html;
-	} else {
-		console.log("x", el);
-		const kids = Array.from(el.childNodes)
-		for (const child of kids) {
-			walk(child);
-		};
-	}
+  if (el.nodeType === Node.TEXT_NODE) {
+    const text = el.textContent;
+    let html = '';
+    const words = text.split(' ');
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      if (word === ' ') {
+        html += ' ';
+      } else {
+        const parts = splitWord(word);
+        for (const part of parts) {
+          html += `<span style="${randomStyle(word)}">${part}</span>`;
+        }
+      }
+    }
+
+    const node = document.createElement('span');
+    el.parentNode.insertBefore(node, el);
+    el.parentNode.removeChild(el);
+    node.outerHTML = html;
+  } else {
+    if (el.tagName === 'IMG') {
+      el.style = `${rndClipPath()}; transform: rotate(${Math.random() * 6 - 3}deg);}`;
+    } else {
+      const kids = Array.from(el.childNodes);
+      for (const child of kids) {
+        walk(child);
+      }
+    }
+  }
 }
 
-function escapeHTML(text) {
-	var textEl = document.createElement("span");
-	textEl.textContent = text;
-	return textEl.innerHTML;
+function splitWord(word) {
+  const parts = [];
+  while (word.length > 0) {
+    const partLength = Math.floor(Math.random() * word.length) + 1;
+    parts.push(word.substring(0, partLength));
+    word = word.substring(partLength);
+  }
+  return parts;
 }
 
 function randomStyle(c) {
-	return `${extraSpace(c)}; ${rndBlur()}; ${rndFont(c)}; ${shadow()}; ${rndbg()}; ${rndTransform()}`;
+  return `${setMargin(c)}; ${setFilter()}; ${setFontFamily(c)}; ${setBoxShadow()}; ${setTransform()}; ${setBackground()}; }`;
 }
 
-function rndTransform() {
-	return `transform: scale(0.9) ${rndTranslate()} rotateY(${Math.random()*80-40}deg) rotateX(${Math.random()*8-4}deg) rotateZ(${Math.random()*16-8}deg)`;
+function setTransform() {
+  return `transform: scale(0.9) ${rndTranslate()} rotateY(${Math.random() * 80 - 40}deg) rotateX(${Math.random() * 8 - 4}deg) rotateZ(${Math.random() * 16 - 8}deg)`;
 }
 
-function extraSpace(c) {
-	if (c.endsWith(" ")) {
-		return `margin-right: 0.5em;`
-	} else {
-		return ``
-	}
+function setMargin(c) {
+  return `margin-right: ${c.endsWith(' ') ? '0.5em;' : 'inherit'}`;
 }
 
 function rndTranslate() {
-	return `translate(${Math.random(60)}px, ${Math.random(60)}px)`
+  return `translate(${Math.random(60)}px, ${Math.random(60)}px)`;
 }
 
-function shadow() {
-	return `box-shadow: ${Math.random()*3}px ${Math.random()*3}px ${3+Math.random()*3}px rgba(0,0,0,0.3);`
+function setBoxShadow() {
+  return `box-shadow: ${Math.random() * 3}px ${Math.random() * 3}px ${3 + Math.random() * 3}px rgba(0,0,0,0.3);`;
 }
 
-const commonFonts = [
-	"Andale Mono",
-	"Arial Black",
-	"Arial",
-	"Avant Garde",
-	"Bookman",
-	"Brush Script MT",
-	"Comic Sans MS",
-	"Courier New",
-	"Engagement",
-	"Garamond",
-	"Georgia",
-	"Helvetica Neue",
-	"Helvetica",
-	"Hoefler Text",
-	"Impact",
-	"Lucida Grande",
-	"Lucida Sans",
-	"Monaco",
-	"Optima",
-	"Palatino",
-	"Plaster",
-	"Schoolbook",
-	"Tahoma",
-	"Times New Roman",
-	"Trebuchet MS",
-	"Verdana"
-  ];
+const commonFonts = ['Andale Mono', 'Arial Black', 'Arial', 'Avant Garde', 'Bookman', 'Brush Script MT', 'Comic Sans MS', 'Courier New', 'Engagement', 'Garamond', 'Georgia', 'Helvetica Neue', 'Helvetica', 'Hoefler Text', 'Impact', 'Lucida Grande', 'Lucida Sans', 'Monaco', 'Optima', 'Palatino', 'Plaster', 'Schoolbook', 'Tahoma', 'Times New Roman', 'Trebuchet MS', 'Verdana'];
 
 const availableFonts = commonFonts.filter(isFontAvailable);
 
 function isFontAvailable(fontName) {
-    const element = document.createElement('span');
-    element.style.fontFamily = fontName;
-    element.style.position = 'absolute';
-    element.style.visibility = 'hidden';
-    element.textContent = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    document.body.appendChild(element);
+  const element = document.createElement('span');
+  element.style.fontFamily = fontName;
+  element.style.position = 'absolute';
+  element.style.visibility = 'hidden';
+  element.textContent = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  document.body.appendChild(element);
 
-    const computedStyle = window.getComputedStyle(element);
-    const isAvailable = computedStyle.fontFamily.toLowerCase().includes(fontName.toLowerCase());
+  const computedStyle = window.getComputedStyle(element);
+  const isAvailable = computedStyle.fontFamily.toLowerCase().includes(fontName.toLowerCase());
 
-    document.body.removeChild(element);
+  document.body.removeChild(element);
 
-    return isAvailable;
+  return isAvailable;
 }
 
-function rndFont(character) {
-    let font = availableFonts[Math.floor(Math.random()*availableFonts.length)];
-    return `font-family: ${font};`;
+function setFontFamily() {
+  const font = availableFonts[Math.floor(Math.random() * availableFonts.length)];
+  return `font-family: ${font};`;
 }
 
-function rndBlur() {
-    return `filter: blur(${Math.random()*1}px)`;
+function setFilter() {
+  return `filter: blur(${Math.random() * 1}px)`;
 }
 
+function rndClipPath() {
+  return `clip-path: polygon(
+    ${Math.random() * 10}% ${Math.random() * 10}%,
+    ${90 + Math.random() * 10}% ${Math.random() * 10}%,
+    ${90 + Math.random() * 10}% ${90 + Math.random() * 10}%,
+    ${Math.random() * 10}% ${90 + Math.random() * 10}%)`;
+}
 
-function rndbg() {
-	const r = 205+Math.random()*50;
-	const g = 205+Math.random()*50;
-	const b = 205+Math.random()*50;
-	return `background-color: rgb(${r}, ${g}, ${b});`;
-//	return `background-color: rgb(${r}, ${g}, ${b}); background-image: url("data:image/svg+xml,%3C!-- svg: first layer --%3E%3Csvg viewBox='0 0 250 250' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='4' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`;
+function setBackground() {
+  const r = 225 + Math.random() * 30;
+  const g = 225 + Math.random() * 30;
+  const b = 225 + Math.random() * 30;
+  return `background-color: rgb(${r}, ${g}, ${b}) !important;`;
 }
 
 walk(document.body);
